3108ae16a833740facb137b1c73ea18f53fb4c3b
William Harrison <william@williamdavidharrison.com.au>
2023-09-11 17:00:29 +0800

    Update messageCreate.ts

--- src/events/guild/messageCreate.ts
+++ src/events/guild/messageCreate.ts
@@ -9,27 +9,27 @@ const event: Event = {
     once: false,
     async execute(client: ExtendedClient, Discord: any, message: Message) {
         try {
-            const requiredPerms: PermissionResolvable = ["SendMessages", "EmbedLinks", "ManageMessages"];
+            const requiredPerms: PermissionResolvable = ["SendMessages", "EmbedLinks"];
 
             if(message.author.bot || !message.guild) return;
             if(!message.guild.members.me.permissions.has(requiredPerms)) return;
 
             if(message.content.toLowerCase().startsWith(main.legacyPrefix) && message.content.length > main.legacyPrefix.length + 3) {
+                const description = [
+                    `👋 Hey there ${message.author}!`,
+                    "\nIn the recent rewrite of the DBH Discord bot we have decided to move away from prefix commands (e.g. `DBH!help`) and have moved to slash commands (e.g. `/help`).",
+                    "\n\nThis change has been made to help make development easier of the Discord bot and allow us to maintain it easily.",
+                    "\nRegards,",
+                    "The **DanBot Team**"
+                ]
+
                 const legacy = new Discord.EmbedBuilder()
                     .setColor(client.config_embeds.default)
                     .setTitle("Deprecation of Prefix Commands")
-                    .setDescription(`
-                        👋 Hey there ${message.author}!
-
-                        In the recent rewrite of the DBH Discord bot we have decided to move away from prefix commands (e.g. \`DBH!help\`) and have moved to slash commands (e.g. \`/help\`).
-
-                        This change has been made to help make development easier of the Discord bot and allow us to maintain it easily.
-
-                        Regards,
-                        The **DanBot Team**
-                    `)
+                    .setDescription(description.join("\n"))
 
                 message.reply({ embeds: [legacy] });
+                return;
             }
         } catch(err) {
             client.logError(err);
