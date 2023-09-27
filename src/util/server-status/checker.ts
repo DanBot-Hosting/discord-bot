c946e8f22a89dfcc2ecb8da0ab373add60c373a1
William Harrison <william@williamdavidharrison.com.au>
2023-09-27 11:42:55 +0800

    feat: ip embed on server status + small changes

--- src/util/server-status/checker.ts
+++ src/util/server-status/checker.ts
@@ -4,32 +4,34 @@ import ping from "ping";
 import updateMessage from "./update-message";
 
 const servers: any = [
-    { name: "PNode 1", ip: "pnode1.danbot.host", type: "performance" },
-    { name: "PNode 2", ip: "pnode2.danbot.host", type: "performance" },
-    { name: "Dono 1", ip: "dono-01.danbot.host", type: "donator" },
-    { name: "Dono 2", ip: "dono-02.danbot.host", type: "donator" },
-    { name: "Dono 3", ip: "dono-03.danbot.host", type: "donator" },
-    { name: "Dono 4", ip: "dono-04.danbot.host", type: "donator" },
-    { name: "Dono 5", ip: "dono-05.danbot.host", type: "donator" },
-    { name: "AU 1", ip: "139.99.171.195", type: "vpn" },
-    { name: "FR 1", ip: "176.31.125.135", type: "vpn" },
-    { name: "US 1", ip: "69.197.129.206", type: "vpn" }
+    { name: "PNode 1", host: "pnode1.danbot.host", type: "performance" },
+    { name: "PNode 2", host: "pnode2.danbot.host", type: "performance" },
+    { name: "Dono 1", host: "dono-01.danbot.host", type: "donator" },
+    { name: "Dono 2", host: "dono-02.danbot.host", type: "donator" },
+    { name: "Dono 3", host: "dono-03.danbot.host", type: "donator" },
+    { name: "Dono 4", host: "dono-04.danbot.host", type: "donator" },
+    { name: "Dono 5", host: "dono-05.danbot.host", type: "donator" },
+    { name: "AU 1", host: "139.99.171.195", type: "vpn" },
+    { name: "FR 1", host: "176.31.125.135", type: "vpn" },
+    { name: "US 1", host: "69.197.129.206", type: "vpn" }
 ]
 
 export default async function (client: ExtendedClient): Promise<ServerStatus[]> {
     const statuses: ServerStatus[] = [];
 
     for(const server of servers) {
-        const res = await ping.promise.probe(server.ip, { timeout: 3 });
+        const res = await ping.promise.probe(server.host, { timeout: 3 });
 
         if(res.alive) {
             statuses.push({
                 server: server,
+                result: res,
                 online: true
             })
         } else {
             statuses.push({
                 server: server,
+                result: res,
                 online: false
             })
         }
@@ -42,8 +44,9 @@ export default async function (client: ExtendedClient): Promise<ServerStatus[]> 
 export type ServerStatus = {
     server: {
         name: string;
-        ip: string;
+        host: string;
         type: string;
     }
+    result: ping.PingResponse;
     online: boolean;
 }
--- src/util/server-status/update-message.ts
+++ src/util/server-status/update-message.ts
@@ -1,5 +1,5 @@
 import ExtendedClient from "../../classes/ExtendedClient";
-import { EmbedBuilder, TextChannel } from "discord.js";
+import { EmbedBuilder, Message, TextChannel } from "discord.js";
 import { ServerStatus } from "./checker";
 
 const serverTypes: any = {
@@ -11,13 +11,15 @@ const serverTypes: any = {
 export default async function (statuses: ServerStatus[], client: ExtendedClient) {
     const channel = await client.channels.fetch(client.config_channels.nodeStatus) as TextChannel;
     const recentMessages = await channel.messages.fetch({ limit: 10 });
-    const message = recentMessages.find(m => m.author.id === client.user.id && m.embeds.length > 0);
+    const message = recentMessages.find((msg: Message) => msg.author.id === client.user.id && msg.embeds.length > 0);
 
-    const embed = new EmbedBuilder()
-        .setTitle("DanBot Hosting Status")
+    const status = new EmbedBuilder()
+        .setTitle("Server Status")
         .setDescription(`<t:${Date.now().toString().slice(0, -3)}:f> (<t:${Date.now().toString().slice(0, -3)}:R>)`)
-        .setFooter({ text: "Updated every 60 seconds." })
         .setTimestamp()
+
+    const nodeIPs = new EmbedBuilder()
+        .setTitle("IP Addresses")
 
     const data: any = {};
 
@@ -29,18 +31,26 @@ export default async function (statuses: ServerStatus[], client: ExtendedClient)
 
             data[type].push({
                 name: status.server.name,
+                ip: status.result.numeric_host,
                 online: status.online
             })
         }
     }
 
     for(const item in data) {
-        embed.addFields({ name: `***${item}***`, value: data[item].map((i: any) => `${i.name}: ${i.online ? "🟢 Online" : "🔴 **Offline**"}`).join("\n"), inline: true });
+        status.addFields({ name: `${item}`, value: data[item].map((item: Server) => `${item.name}: ${item.online ? "🟢 Online" : "🔴 **Offline**"}`).join("\n"), inline: true });
+        nodeIPs.addFields({ name: `${item}`, value: data[item].map((item: Server) => `${item.name}: \`${item.ip}\``).join("\n"), inline: true });
     }
 
     if(message) {
-        await message.edit({ embeds: [embed] });
+        await message.edit({ embeds: [status, nodeIPs] });
     } else {
-        channel.send({ embeds: [embed] });
+        channel.send({ embeds: [status, nodeIPs] });
     }
+}
+
+export type Server = {
+    name: string;
+    ip: string;
+    online: boolean;
 }
