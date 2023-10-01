7e07e1dcd9c18acdc8cae73b0cd9decbf6ff7931
William Harrison <william@williamdavidharrison.com.au>
2023-10-02 07:23:05 +0800

    feat: prepare for migration
    - Update user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,6 +2,9 @@ import { model, Schema } from "mongoose";
 
 const schema = new Schema({
     _id: String,
+    console_id: Number,
+    console_username: String,
+    console_linked: Number,
     hide_premium: Boolean,
     premium_count: Number,
     premium_used: Number
