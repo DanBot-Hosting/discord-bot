b149a92c6093b1b1cd9d24c21caa1a9c049b6cef
William Harrison <william@williamdavidharrison.com.au>
2023-10-06 11:24:32 +0800

    feat: more errors on credit

--- src/util/credit.ts
+++ src/util/credit.ts
@@ -8,6 +8,8 @@ import User from "../models/User";
 export async function add(user: Snowflake, amount: number, client: ExtendedClient): Promise<number> {
     const data = await User.findOne({ _id: user }) || new User({ _id: user, hide_credit: false, credit_amount: 0, credit_used: 0 });
 
+    if(amount <= 0) throw new Error("Cannot add zero or negative credit.");
+
     data.credit_amount += amount;
     await data.save();
 
@@ -46,7 +48,8 @@ export function getPrice(): number {
 export async function remove(user: Snowflake, amount: number, client: ExtendedClient): Promise<number> {
     const data = await User.findOne({ _id: user }) || new User({ _id: user, credit_amount: 0, credit_used: 0 });
 
-    if(data.credit_amount < amount) throw new Error(`<@${user}> does not have that much credot to remove.`);
+    if(amount <= 0) throw new Error("Cannot remove zero or negative credit.");
+    if(data.credit_amount < amount) throw new Error(`<@${user}> does not have that much credit to remove.`);
     if(data.credit_used > data.credit_amount - amount) throw new Error(`Cannot remove credit from <@${user}> that is in use.`);
 
     data.credit_amount -= amount;
@@ -65,6 +68,7 @@ export async function remove(user: Snowflake, amount: number, client: ExtendedCl
 export async function set(user: Snowflake, amount: number, client: ExtendedClient): Promise<number> {
     const data = await User.findOne({ _id: user }) || new User({ _id: user, credit_amount: 0, credit_used: 0 });
 
+    if(amount < 0) throw new Error("Cannot set credit to a negative amount.");
     if(data.credit_used > amount) throw new Error(`You cannot set <@${user}>'s credit amount to less than they have used.`);
 
     data.credit_amount = amount;
