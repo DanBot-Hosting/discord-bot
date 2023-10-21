2706717ba7793ae8aa96ceba0168330ccf6df5b9
William Harrison <william@williamdavidharrison.com.au>
2023-10-22 07:46:12 +0800

    Update debug.ts

--- src/commands/bot-admin/debug.ts
+++ src/commands/bot-admin/debug.ts
@@ -29,10 +29,6 @@ const command: Command = {
                     { name: "🖨️ Memory Usage", value: formatBytes(mem, 0), inline: true }
                 )
 
-            // session_start_limit
-            // https://discord.com/developers/docs/topics/gateway#session-start-limit-object-session-start-limit-structure
-            const session: APIGatewaySessionStartLimit = (await client.rest.get("/gateway/bot") as any).session_start_limit;
-
             const bot = new Discord.EmbedBuilder()
                 .setColor(client.config_embeds.default)
                 .setTitle("🤖 Bot")
@@ -40,7 +36,6 @@ const command: Command = {
                     { name: `${emoji.nodejs} Node.js`, value: `${process.version}`, inline: true },
                     { name: `${emoji.discordjs} Discord.js`, value: `v${Discord.version}`, inline: true },
                     { name: "🟢 Online Since", value: `<t:${(Date.now() - client.uptime).toString().slice(0, -3)}:f> (<t:${(Date.now() - client.uptime).toString().slice(0, -3)}:R>)` },
-                    { name: "📈 Session Limit", value: `${session.total - session.remaining} **/** ${session.total} (resets <t:${((Date.now() + session.reset_after) / 1000).toString().slice(0, -4)}:R>)`, inline: true },
                     { name: "🌐 Websocket Ping", value: `${client.ws.ping}ms`, inline: true },
                     { name: "🔊 Max Listeners", value: `${client.getMaxListeners()}`, inline: true },
                     { name: `${emoji.discord} Commands`, value: `**${client.commands.size}** Slash Commands\n**${client.legacyCommands.size}** Legacy Commands` }
