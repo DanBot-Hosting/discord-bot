a641542b290497d98031b725b34af91ef3305162
William Harrison <william@williamdavidharrison.com.au>
2023-12-29 17:12:43 +0800

    Create delete-bot-api-key.ts

---
+++ src/commands/beta-testers/delete-bot-api-key.ts
@@ -0,0 +1,64 @@
+import Command from "../../classes/Command";
+import ExtendedClient from "../../classes/ExtendedClient";
+import { CommandInteraction } from "discord.js";
+
+import axios from "axios";
+import { emojis as emoji } from "../../config";
+
+const command: Command = {
+    name: "delete-bot-api-key",
+    description: "Delete your API key from the bot API.",
+    options: [],
+    default_member_permissions: null,
+    botPermissions: [],
+    requiredRoles: ["betaTester"],
+    cooldown: 120,
+    enabled: true,
+    deferReply: true,
+    ephemeral: true,
+    async execute(interaction: CommandInteraction, client: ExtendedClient, Discord: typeof import("discord.js")) {
+        try {
+            const deleting = new Discord.EmbedBuilder()
+                .setColor(client.config_embeds.default)
+                .setDescription(`${emoji.ping} Deleting API key...`)
+
+            await interaction.editReply({ embeds: [deleting] });
+
+            let data = null;
+
+            try {
+                const result = await axios.post(`https://${client.config_main.botAPIDomain}/removeapikey?discordid=${interaction.user.id}&key=${process.env.bot_api_master_key}`, {
+                    headers: {
+                        "User-Agent": "DBH"
+                    }
+                })
+
+                data = result.data;
+            } catch(err) {
+                data = err.response.data;
+            }
+
+            if(data.error) {
+                const error = new Discord.EmbedBuilder()
+                    .setColor(client.config_embeds.error)
+                    .setDescription(`${emoji.cross} An error occurred while deleting your API key.`)
+                    .addFields (
+                        { name: "Error Message", value: `\`\`\`${data.error}\`\`\`` }
+                    )
+
+                await interaction.editReply({ embeds: [error] });
+                return;
+            }
+
+            const deleted = new Discord.EmbedBuilder()
+                .setColor(client.config_embeds.default)
+                .setDescription(`${emoji.tick} Your API key has been deleted!`)
+
+            await interaction.editReply({ embeds: [deleted] });
+        } catch(err) {
+            client.logCommandError(err, interaction, Discord);
+        }
+    }
+}
+
+export = command;
