a05dbb1298fcd45bcc7e1631ae4b3f5449027de8
William Harrison <william@williamdavidharrison.com.au>
2024-01-27 09:13:08 +0800

    feat: update vc stats

--- src/util/vcStats.ts
+++ src/util/vcStats.ts
@@ -13,24 +13,21 @@ export default async function (client: ExtendedClient) {
     const members = guild.channels.cache.get(channel.members) as VoiceChannel;
     const staff = guild.channels.cache.get(channel.staff) as VoiceChannel;
     const tickets = guild.channels.cache.get(channel.tickets) as VoiceChannel;
-    const totalMembers = guild.channels.cache.get(channel.totalMembers) as VoiceChannel;
 
     const ticketCategories = Object.values(categories.tickets);
 
     const stats = {
         boosts: guild.premiumSubscriptionCount,
         bots: guild.members.cache.filter(member => member.user.bot).size,
-        members: guild.members.cache.size,
+        members: guild.memberCount,
         staff: guild.members.cache.filter(member => member.roles.cache.has(client.config_roles.staff)).size,
-        tickets: guild.channels.cache.filter(channel => channel.name.startsWith("🎫╏") && ticketCategories.includes(channel.parentId)).size,
-        totalMembers: guild.memberCount
+        tickets: guild.channels.cache.filter(channel => channel.name.startsWith("🎫╏") && ticketCategories.includes(channel.parentId)).size
     }
 
     // Update stats
-    await boosts.setName(`Boosts: ${stats.boosts}`);
-    await bots.setName(`Bots: ${stats.bots}`);
-    await members.setName(`Members: ${stats.members}`);
-    await staff.setName(`Staff: ${stats.staff}`);
-    await tickets.setName(`Tickets: ${stats.tickets}`);
-    await totalMembers.setName(`Total Members: ${stats.totalMembers}`);
+    await boosts.setName(`Boosts » ${stats.boosts}`);
+    await bots.setName(`Bots » ${stats.bots}`);
+    await members.setName(`Members » ${stats.members}`);
+    await staff.setName(`Staff » ${stats.staff}`);
+    await tickets.setName(`Tickets » ${stats.tickets}`);
 }
